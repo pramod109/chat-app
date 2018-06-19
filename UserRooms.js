848ef1d77b0f26d99da948879149b1de37fe8695
pramod hanagandi <pramodsh109@gmail.com>
2018-06-19 16:28:26 -0500

    Developed the initial skeleton

---
+++ UserRooms.js
@@ -0,0 +1,78 @@
+class UserRooms{
+    constructor(){
+        this.userRooms = [
+            {
+                name: 'Test room 1',
+                users: [],
+                messages: []
+            },
+            {
+                name: 'Test room 2',
+                users: [],
+                messages: []
+            },
+            {
+                name: 'Test room 3',
+                users: [],
+                messages: []
+            }
+        ];
+    }
+
+    getUserRooms(){
+        return this.userRooms;
+    }
+
+    getUserRoom(roomName){
+        return this.userRooms.find(room => room.name === roomName);
+    }
+
+    addUserRoom(roomName){
+        if(!this.userRooms.find(room => room.name === roomName)){
+            this.userRooms.push({
+                name: 'roomName',
+                users: [],
+                messages: []
+            })
+        }
+    }
+
+    removeUserRoom(roomName){
+        this.userRooms = this.userRooms.filter((room) => {
+            if(roomName !== 'Test room'){
+                return room.name !== roomName;
+            }
+
+            return room;
+        })
+    }
+
+    addUser(userName, roomName){
+        const room = this.userRooms.find(room => room.name === roomName);
+
+        if(!room.users.find(user => user === userName)){
+            room.users.push(userName);
+        }
+    }
+
+    removeUser(userName, roomName){
+        const room = this.getUserRoom(roomName);
+
+        if(room){
+            room.users = room.users.filter(user => user !== userName);
+
+            if(!room.users.length){
+                this.removeUserRoom(roomName);
+            }
+        }
+    }
+
+    addMessage(message,roomName){
+        const room = this.getUserRoom(roomName);
+
+        if(room){
+            room.messages.push(message);
+        }
+    }
+}
+module.exports = { UserRooms };
--- server.js
+++ server.js
@@ -1,20 +1,48 @@
 /* 
 Developed by Pramod Hanagandi -[pramod109.github.io]
 This page handles the backend requests for the chat application
+Reference: https://socket.io/get-started/chat/
 */
 
-var express = require('express');
-var socket = require('socket.io');
-var app = express();
+const http = require('http');
+const path = require('path');
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const clientPath = path.join(__dirname, 'client-files');
+const express = require('express');
+const { UserRooms } = require('./UserRooms');
 
+const user_rooms = new UserRooms();
 
+app.use(express.static(clientPath));
 
-server = app.listen(process.env.PORT || 3003, () =>{
-	console.log("Server active on port 3003");
+app.get('/', function(req,res){
+	res.sendFile(path.join(clientPath, 'index.html'));
 });
 
-io = socket(server);
-
-io.on('connection', (socket)=>{
-	console.log("New socket created");
+io.on('connection', function(socket){
+	console.log("A user connected...")
+	
+	socket.on('disconnect', function(){
+		console.log("User disconnected...")
+	});
+	
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
 });
+
+app.get('/client', function(req, res){
+	res.sendFile(path.join(clientPath, 'client-pages', 'client-chat', 'client-chat.html'));
+})
+
+app.get('/admin', function(req, res){
+	// res.send(user_rooms.getUserRooms());
+	// console.log("Printing rooms...");
+	// console.log(user_rooms.getUserRooms());
+	
+	res.sendFile(path.join(clientPath, 'admin-pages', 'admin-chat', 'admin-chat.html'));
+});
+
+server.listen(3000,()=>{console.log('Server active...')});
