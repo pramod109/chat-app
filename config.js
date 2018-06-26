24f462b0d9463f89687eff7462ca897ab77fc902
pramod hanagandi <pramodsh109@gmail.com>
2018-06-25 21:15:37 -0500

    Edited config.js for database connections

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
+//please update this file according to your database
+
 module.exports = {
     'secret' : 'chatappsecret',
-    'database' : 'mongodb://pramod:pramod321@ds163870.mlab.com:63870/chat-app'
+    'database' : 'mongodb://<dbuser>:<dbpassword>@ds163870.mlab.com:63870/<dbname>'
 }
