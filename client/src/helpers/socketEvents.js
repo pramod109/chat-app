9ace464ac0f8bc23b922a894c2a6ae899faf7471
pramod hanagandi <pramodsh109@gmail.com>
2018-06-25 20:00:40 -0500

    Debugging Heroku deployment errors-'cannot get '/'

--- client/src/helpers/socketEvents.js
+++ client/src/helpers/socketEvents.js
@@ -1,6 +1,7 @@
 import socketIOClient from 'socket.io-client';
 
-const socket = socketIOClient('http://localhost:3001');
+//http://localhost:3001
+const socket = socketIOClient('/');
 
 const socketOn = {
     chatMessage: (callback) => {
